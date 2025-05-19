238c4ec04f79a53915de3c5c4985cd6413e11068
daniel <danielrodriguess878@hotmail.com>
2025-05-18 23:58:11 -0300

    inserção do botão administrativo

--- estacionamento/urls.py
+++ estacionamento/urls.py
@@ -4,10 +4,9 @@ from . import views
 app_name = 'estacionamento'
 
 urlpatterns = [
-    path('', views.lista_veiculos, name='lista_veiculos'),
+    path('', views.adicionar_veiculo, name='adicionar_veiculo'), 
+    path('lista/', views.lista_veiculos, name='lista_veiculos'),  
     path('adicionar/', views.adicionar_veiculo, name='adicionar_veiculo'),
     path('saida/<int:veiculo_id>/', views.registrar_saida, name='registrar_saida'),
-    path('veiculos/', views.lista_veiculos, name='lista_veiculos'),
-    path('historico/', views.historico_veiculos, name='historico'),
     path('historico/', views.historico_veiculos, name='historico'),
 ]
