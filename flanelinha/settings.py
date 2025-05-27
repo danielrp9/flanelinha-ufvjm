25a2f6becb1305757fdb477cd4f9e4d776f719a6
daniel <danielrodriguess878@hotmail.com>
2025-05-27 09:03:52 -0300

    atualização de render

--- flanelinha/settings.py
+++ flanelinha/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME', 'localhost')]
+#ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
