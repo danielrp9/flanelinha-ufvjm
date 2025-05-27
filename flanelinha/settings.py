178a593c8d801e25ae31fc71d2f5078c26509892
daniel <danielrodriguess878@hotmail.com>
2025-05-27 09:58:07 -0300

    atualização sqlite

--- flanelinha/settings.py
+++ flanelinha/settings.py
@@ -17,6 +17,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -82,8 +83,7 @@ WSGI_APPLICATION = 'flanelinha.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
