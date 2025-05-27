96ffed118bb13e2f62b9c582c404f2d407559c64
daniel <danielrodriguess878@hotmail.com>
2025-05-27 08:40:19 -0300

    atualização para o render -3

--- flanelinha/settings.py
+++ flanelinha/settings.py
@@ -142,3 +142,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
